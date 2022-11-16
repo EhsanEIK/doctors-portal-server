f64ab5eb935d9390c3c375e2ba71b2f1de3995a0
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-16 09:25:27 +0600

    added connection between server and database

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
+require('dotenv').config();
 
 // middleware
 app.use(cors());
+app.use(express.json());
+
+// database work
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fbieij7.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try { }
+    finally { }
+}
+
+run().catch(error => console.error(error));
 
 app.get('/', (req, res) => {
     res.send('Doctors Portal Server is running');
