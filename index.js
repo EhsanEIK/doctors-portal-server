61ac0cef57928c63bbb48f9dbc245496c353d901
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-17 11:31:35 +0600

    added verifyJWT token function and verify bookings GET api using jwt token

--- index.js
+++ index.js
@@ -10,6 +10,22 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: "unauthorized access" });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: "forbidden access" });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 // database work
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fbieij7.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -50,8 +66,12 @@ async function run() {
         })
 
         // bookings [GET]
-        app.get('/bookings', async (req, res) => {
+        app.get('/bookings', verifyJWT, async (req, res) => {
             const email = req.query.email;
+            const decodedEmail = req.decoded.email;
+            if (email !== decodedEmail) {
+                return res.status(403).send({ message: "forbidden access" });
+            }
             const query = { email: email };
             const bookings = await bookingsCollection.find(query).toArray();
             res.send(bookings);
